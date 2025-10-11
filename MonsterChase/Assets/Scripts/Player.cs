2789b6659f65e467bcb2e403a83e20593ae3b495
sham197 <samilzp@gmail.com>
2025-10-11 17:02:02 +0300

    Refactor player input and controls for two players
    Added auto-generated PlayerInput.cs and updated input actions to support movement and jumping. Refactored Player.cs to use the new input system, handle movement and jump via events, and improved animation logic. Updated Player1 and Player2 prefabs and Gameplay scene to support two players with independent input and physics. Adjusted walk animation sample rates and controller speeds for smoother movement.

--- MonsterChase/Assets/Scripts/Player.cs
+++ MonsterChase/Assets/Scripts/Player.cs
@@ -2,32 +2,93 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float moveForce = 10f;
-    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] float moveForce = 5f;
+    [SerializeField] float jumpForce = 10f;
 
-    private Vector2 _moveInput;
-    [SerializeField] Rigidbody2D _rigidbody2D;
-    [SerializeField] private SpriteRenderer _spriteRenderer;
+    Vector2 _moveInput;
+    Rigidbody2D _rigidbody2D;
+    SpriteRenderer _spriteRenderer;
     
-    [SerializeField] private Animator _anim;
-    
-    private string WALK_ANIMATION = "walk";
+    Animator _anim;
+    PlayerInput _playerInput;
+    bool _isGrounded = true;
+    string WALK_ANIMATION = "Walk";
     private void Awake()
     {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _anim = GetComponent<Animator>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _playerInput = new PlayerInput();
+        
+        // Подписка на события
+        _playerInput.Player.Move.performed += OnMove;
+        _playerInput.Player.Move.canceled += OnMove;
+        
+        _playerInput.Player.Jump.performed += OnJump;
+    }
+    private void OnEnable()
+    {
+        _playerInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _playerInput.Disable();
+    }
+
+    private void OnDestroy()
+    {
+        // Отписка от событий
+        _playerInput.Player.Move.performed -= OnMove;
+        _playerInput.Player.Move.canceled -= OnMove;
+        _playerInput.Player.Jump.performed -= OnJump;
     }
 
     void OnMove(InputAction.CallbackContext context)
     {
         _moveInput = context.ReadValue<Vector2>();
-        Debug.Log(_moveInput);
+    }
+
+    void OnJump(InputAction.CallbackContext context)
+    {
+        if (_isGrounded)
+        {
+            _rigidbody2D.linearVelocity = new Vector2(_rigidbody2D.linearVelocity.x, 0f);
+            _rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
     
-    void MovePlayerKeyboard()
-    {
-        transform.position += new Vector3(_moveInput.x, 0f, 0f) * moveForce * Time.deltaTime;
-    }
     void Update()
     {
-        MovePlayerKeyboard();
+        AnimatePlayer();
     }
+    void FixedUpdate()
+    {
+        MovePlayerPhysics();
+    }
+
+    void MovePlayerPhysics()
+    {
+        // Устанавливаем скорость напрямую (сохраняя вертикальную)
+        _rigidbody2D.linearVelocity = new Vector2(_moveInput.x * moveForce, _rigidbody2D.linearVelocity.y);
+    }
+    
+    void AnimatePlayer()
+    {
+        if (_moveInput.x > 0)
+        {
+            _anim.SetBool(WALK_ANIMATION, true);
+            _spriteRenderer.flipX = false;
+        }
+        else if (_moveInput.x < 0)
+        {
+            _anim.SetBool(WALK_ANIMATION, true);
+            _spriteRenderer.flipX = true;
+        }
+        else
+        {
+            _anim.SetBool(WALK_ANIMATION, false);
+        }
+    }
+
 }
